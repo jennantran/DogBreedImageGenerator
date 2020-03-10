4e1e19afe13b5236b38c1d78f3de99415b7fdfb0
Jennifer Tran <jennantran@gmail.com>
2020-03-10 16:37:51 -0700

    made changes for user typing in a non-breed

--- index.js
+++ index.js
@@ -8,16 +8,23 @@ function getDogBreedImage() {
       .then(response => response.json())
       .then(responseJson => 
         displayResults(responseJson))
-      .catch(error => alert('Something went wrong. Try again later.'));
+      .catch(error => {
+        console.log(error);
+      })
   }
 
 function displayResults(responseJson) {
     console.log(responseJson);
-    $('.results').replaceWith(
-      `<img src="${responseJson.message}" class="results">`
-    )
-    $('.results').removeClass('hidden');
-  }
+    //check code range is in 200 and less 300
+    if(responseJson.status == "success"){
+        $('.results').replaceWith(
+          `<img src=${responseJson.message} class="results">`);
+        $('.results').removeClass('hidden');
+    }else {
+        alert(responseJson.message);
+    }
+}
+
   
   function watchForm() {
     $('form').submit(event => {
